eb19345bb43aef2814b0bcfd651ba2e81a4534ca
Thomas Perl <thp@thpinfo.com>
2010-10-10 12:57:06 +0200

    mygpoclient 1.5 "The Hunting Party" released
    This is a bugfix release. No new features.

--- mygpoclient/__init__.py
+++ mygpoclient/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '1.4'
+__version__ = '1.5'
 __url__ = 'http://thpinfo.com/2010/mygpoclient/'
 
 # Default settings for the API client (server hostname and API version)
