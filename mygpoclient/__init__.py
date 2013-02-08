f59633e181802d6001e37a7d39373156b6f1073e
Thomas Perl <m@thp.io>
2013-02-08 12:50:48 +0100

    mygpoclient 1.7 "Nuisance Value" released

--- mygpoclient/__init__.py
+++ mygpoclient/__init__.py
@@ -21,7 +21,7 @@ This is mygpoclient, the gpodder.net API Client Library for Python.
 """
 
 __author__ = 'Thomas Perl <thp@gpodder.org>'
-__version__ = '1.6'
+__version__ = '1.7'
 __website__ = 'http://thp.io/2010/mygpoclient/'
 __license__ = 'GNU General Public License v3 or later'
 
